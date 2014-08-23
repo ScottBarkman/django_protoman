cf51b21dbef1955a598ef40c6ea5c5023d11c7e4
= <scott@liftinteractive.com>
2014-08-22 21:43:25 -0600

    i suck

--- django-protoman/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from .views import *
-
-urlpatterns = patterns(
-    "prototyper.urls",
-    url(r'^awesomesauce/$', Awesomesauce.as_view(), name='awesomesauce'),
-    url(r'^awesomesauce/$', Awesomesauce.as_view(), name='awesomesauce'),
-    url(r'^awesomesauce/$', Awesomesauce.as_view(), name='awesomesauce'),
-)
